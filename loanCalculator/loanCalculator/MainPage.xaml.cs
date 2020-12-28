c45161d6d8f190de8465dd38b32e3a47bda47045
Parker <kirbypar@msu.edu>
2020-12-28 18:07:49 -0500

    Add project files.

---
+++ loanCalculator/loanCalculator/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace loanCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button temp  = this.FindByName<Button>("tempButton");
+            temp.BackgroundColor = Color.Red;
+
+
+        }
+    }
+}
